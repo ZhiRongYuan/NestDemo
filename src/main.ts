03ada4f608feff23ef187a14c7696fcdd5319644
yuanzhirong <283211086@qq.com>
2023-11-03 15:42:39 +0800

    利用Pipe通道实现数据校验

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@
  * @Author: yuanzhirong
  * @Date: 2023-10-31 11:56:08
  * @LastEditors: yuanzhirong
- * @LastEditTime: 2023-11-01 11:31:32
- * @Description: 
+ * @LastEditTime: 2023-11-03 12:01:10
+ * @Description:
  */
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { generateDocument } from './doc';
@@ -12,6 +13,10 @@ import { generateDocument } from './doc';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 添加数据校验
+  app.useGlobalPipes(new ValidationPipe());
+
+  // 创建swagger文档
   generateDocument(app);
   await app.listen(3000);
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,23 +2,29 @@
  * @Author: yuanzhirong
  * @Date: 2023-11-03 11:23:43
  * @LastEditors: yuanzhirong
- * @LastEditTime: 2023-11-03 11:39:54
+ * @LastEditTime: 2023-11-03 11:59:27
  * @Description:
  */
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator';
 
 export class CreateUserDto {
   /**
    * 手机号（系统唯一）
    */
   @ApiProperty({ example: '18166049498' })
+  @Matches(/^1\d{10}$/g, { message: '请输入手机号' })
   readonly phoneNumber: string;
 
-  @ApiProperty({ example: '亲子手工艺' }) name: string;
+  @ApiProperty({ example: '亲子手工艺' })
+  name: string;
 
   @ApiProperty({ example: '123456' })
+  @IsNotEmpty()
+  @Length(6,10)
   password: string;
 
   @ApiProperty({ example: '283211086@qq.com' })
+  @IsEmail()
   email: string;
 }
