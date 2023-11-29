a0cfcfe02e3dfcf3d2fe9071926f8c28acc57143
yuanzhirong <283211086@qq.com>
2023-11-29 17:24:47 +0800

    增加环境变量

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
 
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: envFilePath,
+      load: [() => dotenv.config({ path: '.env' })],
     }),
     UserModule,
   ],
